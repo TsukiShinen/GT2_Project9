d669d62e34aedce470c930cdcd383058b7e39061
TsukiShinen <49118528+TsukiShinen@users.noreply.github.com>
2022-03-29 09:42:09 +0200

    Appel des fonction

--- Assets/Scripts/ScriptableObjects/InputEvent.cs
+++ Assets/Scripts/ScriptableObjects/InputEvent.cs
@@ -8,9 +8,11 @@ public class InputEvent : ScriptableObject
     private Tank _selectedTank;
     [SerializeField] private GameParameters _parameters;
 
+    private Vector2 MousePosition => Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
     public void OnRightClick()
     {
-        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.forward);
+        RaycastHit2D hit = Physics2D.Raycast(MousePosition, Vector3.forward);
         if (hit.collider == null) { return; }
 
         if (_selectedTank) { 
@@ -28,20 +30,21 @@ public class InputEvent : ScriptableObject
     private void SelectedTank(RaycastHit2D hit)
     {
         if (!hit.collider.CompareTag(_parameters.TagTank)) { return; }
-        Debug.Log("Select Tank");
         _selectedTank = hit.collider.gameObject.GetComponent<Tank>();
     }
 
     private void UnSelectTank()
     {
-        Debug.Log("UnSelect Tank");
         _selectedTank = null;
     }
 
     private void Action(RaycastHit2D hit)
     {
-        if (hit.collider.CompareTag(_parameters.TagTank)) { Debug.Log("Attack"); }
-        else { Debug.Log("Move"); }
+        if (hit.collider.CompareTag(_parameters.TagTank)) { 
+            // _selectedTank.Attack(hit.collider.gameObject); 
+        } else { 
+            // _selectedTank.GoTo(MousePosition); 
+        }
         UnSelectTank();
     }
 }
