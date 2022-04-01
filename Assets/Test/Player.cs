461123cad0cfe61d66ea6ca0fceba0fd34274251
AokoMitsUwU <80577298+AokoMitsuu@users.noreply.github.com>
2022-04-01 15:07:15 +0200

    FOW

---
+++ Assets/Test/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f) * movementSpeed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
