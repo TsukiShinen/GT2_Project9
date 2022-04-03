f1545978571fc3598d5a5f66c0687d1e478e655b
TsukiShinen <49118528+TsukiShinen@users.noreply.github.com>
2022-04-03 12:59:20 +0200

    Fix FPS

--- Assets/Scripts/Actions/ActionGoTo.cs
+++ Assets/Scripts/Actions/ActionGoTo.cs
@@ -10,6 +10,7 @@ public class ActionGoTo : IAction
         if (tank == null) { return; }
         Vector3 positionToGo = (Vector3)args[1];
 
+        if (tank.PositionToGo == positionToGo) { return; }
         tank.PositionToGo = positionToGo;
         tank.GridController.GenerateFlowField(positionToGo);
         tank.NextState = "GoTo";
