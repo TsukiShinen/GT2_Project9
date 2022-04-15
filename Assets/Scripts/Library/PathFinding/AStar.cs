cfb4fe327b321f16c5839f681dba0d9981e79ee8
AokoMitsUwU <80577298+AokoMitsuu@users.noreply.github.com>
2022-04-15 19:34:52 +0200

    astar endo

--- Assets/Scripts/Library/PathFinding/AStar.cs
+++ Assets/Scripts/Library/PathFinding/AStar.cs
@@ -5,159 +5,63 @@ namespace PathFinding
 {
 	public class AStar : PathFinding
 	{
-        private List<Cell> _cellsFound;
-        private List<Cell> _cells;
-        private List<Cell> _cellsChecked;
-
-        private Cell _origin;
-        private Cell _destination;
-        private List<Vector3> _path;
         public override Queue<Vector3> GeneratePath(Vector2 startingPosition, Vector2 destinationPosition)
-		{
-			CreateAStar(startingPosition, destinationPosition);
-            return Path;
-		}
-
-        public void CreateAStar(Vector2 startingPosition, Vector2 destinationPosition)
         {
-            _cellsFound = new List<Cell>();
-            _cellsChecked = new List<Cell>();
-            _path = new List<Vector3>();
+            Dictionary<Cell, Cell> NextCellToGoal = new Dictionary<Cell, Cell>();//Determines for each Cell where you need to go to reach the goal. Key=Cell, Value=Direction to Goal
+            Dictionary<Cell, int> costToReachCell = new Dictionary<Cell, int>();//Total Movement Cost to reach the Cell
 
-            var initialPos = GetCellFromWorldPosition(startingPosition);
-            _cells = GetNeighborCells(initialPos.GridIndex, GridDirection.CardinalAndInterCardinalDirections);
-            _origin = initialPos;
-            _destination = GetCellFromWorldPosition(destinationPosition);
-            _cellsFound.Add(Grid[initialPos.GridIndex.x, initialPos.GridIndex.y]);
-            InitialToDest(initialPos.GridIndex);
-        }
+            StartingCell = GetCellFromWorldPosition(startingPosition);
+            DestinationCell = GetCellFromWorldPosition(destinationPosition);
 
-        private float CheckHTile(Cell cell)
-        {
-            float cost = 0;
-            var position = Vector2.zero;
-            var distance = new Vector2(Mathf.Abs(cell.GridIndex.x - _destination.GridIndex.x), Mathf.Abs(cell.GridIndex.y - _destination.GridIndex.y));
-            while (position.x < distance.x && position.y < distance.y)
-            {
-                position.x += 1;
-                position.y += 1;
-                cost += 14;
-            }
-            if (position.x < distance.x)
-            {
-                cost += 10 * (distance.x - position.x);
-            }
-            if (position.y < distance.y)
-            {
-                cost += 10 * (distance.y - position.y);
-            }
-            return cost;
-        }
+            PriorityQueue<Cell> frontier = new PriorityQueue<Cell>();
+            frontier.Enqueue(DestinationCell, 0);
+            costToReachCell[DestinationCell] = 0;
 
-        private float CheckGTile(Cell cell, Vector2Int initialPos)
-        {
-            float cost = 0;
-            var position = Vector2.zero;
-            var distance = new Vector2(Mathf.Abs(cell.GridIndex.x - initialPos.x), Mathf.Abs(cell.GridIndex.y - initialPos.y));
-            while (position.x < distance.x && position.y < distance.y)
+            while (frontier.Count > 0)
             {
-                position.x += 1;
-                position.y += 1;
-                cost += 14;
-            }
-            if (position.x < distance.x)
-            {
-                cost += 10 * (distance.x - position.x);
-            }
-            if (position.y < distance.y)
-            {
-                cost += 10 * (distance.y - position.y);
-            }
-            return cost;
-        }
+                Cell curCell = frontier.Dequeue();
+                if (curCell == StartingCell)
+                    break;
 
-        private void AddToList(Cell cell)
-        {
-            var tmpList = GetNeighborCells(cell.GridIndex, GridDirection.CardinalAndInterCardinalDirections);
-            foreach (var neighborCell in tmpList)
-            {
-                if (!_cells.Contains(neighborCell) && neighborCell.Cost < 255 && !_cellsChecked.Contains(neighborCell))
+                foreach (Cell neighbor in GetNeighborCells(curCell.GridIndex, GridDirection.CardinalAndInterCardinalDirections))
                 {
-                    _cells.Add(neighborCell);
+                    int newCost = costToReachCell[curCell] + neighbor.Cost;
+                    if (costToReachCell.ContainsKey(neighbor) == false || newCost < costToReachCell[neighbor])
+                    {
+                        if (neighbor.Cost < 255)
+                        {
+                            costToReachCell[neighbor] = newCost;
+                            int priority = newCost + Distance(neighbor, StartingCell);
+                            frontier.Enqueue(neighbor, priority);
+                            NextCellToGoal[neighbor] = curCell;
+                            //neighbor._Text = costToReachCell[neighbor].ToString();
+                        }
+                    }
                 }
             }
-        }
 
-        private void AddToList2(Cell cell)
-        {
-            var tmpList = GetNeighborCells(cell.GridIndex, GridDirection.CardinalAndInterCardinalDirections);
-            foreach (var neighborCell in tmpList)
-            {
-                if (!_cells.Contains(neighborCell) && neighborCell.Cost < 255 && !_cellsChecked.Contains(neighborCell) && _cellsFound.Contains(neighborCell))
-                {
-                    _cells.Add(neighborCell);
-                }
-            }
-        }
+            //Get the Path
 
-        private Cell FindMinimalCell(Vector2Int initialPos)
-        {
-            var cost = Mathf.Infinity;
-            var index = 0;
-            for (var x = 0; x < _cells.Count; x++)
+            //check if Cell is reachable
+            if (NextCellToGoal.ContainsKey(StartingCell) == false)
             {
-                if (CheckHTile(_cells[x]) + CheckGTile(_cells[x], initialPos) < cost)
-                {
-                    cost = CheckHTile(_cells[x]) + CheckGTile(_cells[x], initialPos);
-                    index = x;
-                }
+                return null;
             }
-            var tmpCell = _cells[index];
-            return tmpCell;
-        }
-        private void InitialToDest(Vector2Int initialPos)
-        {
-            var tmpCell = FindMinimalCell(initialPos);
-            _cells.Remove(tmpCell);
-            _cellsChecked.Add(tmpCell);
-            AddToList(tmpCell);
-            _cellsFound.Add(tmpCell);
-            if (tmpCell.GridIndex == _destination.GridIndex)
-            {
-                _destination = _origin;
-                _cells.Clear();
-                _cellsChecked.Clear();
-                AddToList2(tmpCell);
-                DestToInitial(tmpCell.GridIndex);
 
-            }
-            else
+            Queue<Vector3> path = new Queue<Vector3>();
+            Cell pathCell = StartingCell;
+            while (DestinationCell != pathCell)
             {
-                InitialToDest(tmpCell.GridIndex);
+                pathCell = NextCellToGoal[pathCell];
+                path.Enqueue(pathCell.WorldPosition);
             }
-        }
+            return path;
 
-        private void DestToInitial(Vector2Int initialPos)
-        {
-            if (initialPos == new Vector2Int(2, 2))
+            int Distance(Cell c1, Cell c2)
             {
-                Debug.Log("test");
+                return (int) (Mathf.Abs(c1.WorldPosition.x - c2.WorldPosition.x) + Mathf.Abs(c1.WorldPosition.y - c2.WorldPosition.y));
             }
-            var tmpCell = FindMinimalCell(initialPos);
-            _cells.Remove(tmpCell);
-            _cellsChecked.Add(tmpCell);
 
-            AddToList2(tmpCell);
-            _path.Add(tmpCell.WorldPosition);
-            if (tmpCell.GridIndex != _destination.GridIndex)
-            {
-                DestToInitial(tmpCell.GridIndex);
-            }
-            else
-            {
-                _path.Reverse();
-                Path = new Queue<Vector3>(_path);
-            }
         }
     }
 }
